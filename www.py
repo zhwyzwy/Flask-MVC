7c4c25f0d868f7299f86d7d4de1cca668bad2ff9
zhwy <zhwy@test.com>
2020-09-04 14:48:54 +0800

    HTTP模块相关初始化

---
+++ www.py
@@ -0,0 +1,7 @@
+from application import app
+
+from web.controllers.index import route_index
+from web.controllers.user.User import route_user
+
+app.register_blueprint(route_index, url_prefix='/')
+app.register_blueprint(route_user, url_prefix='/user')
